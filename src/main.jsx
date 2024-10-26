d29e72ffb4d82c5c143625325908aec41a6ed664
Prashant <prashant@Prashants-MacBook-Air.local>
2024-10-26 22:54:56 +0530

    Some updates

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+// Google Analytics Code
+const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+(function (w, d, s, g, js, fjs) {
+  g = w.gtag || function () {
+    (g.q = g.q || []).push(arguments);
+  };
+  js = d.createElement(s);
+  js.src = 'https://www.googletagmanager.com/gtag/js?id=' + gaMeasurementId;
+  fjs = d.getElementsByTagName(s)[0];
+  fjs.parentNode.insertBefore(js, fjs);
+  g('js', new Date());
+  g('config', gaMeasurementId);
+})(window, document, 'script');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
